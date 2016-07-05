2abe798110b27f26e2afb7fdd9a06af255fe9c88
eugenechen0514 <yuchiehchen@outlook.com>
2016-07-06 01:00:11 +0800

    Add more test

--- ProductTest/Tool/CalculatorTests.cs
+++ ProductTest/Tool/CalculatorTests.cs
@@ -11,7 +11,7 @@ namespace ProductTest.Tool
     public class CalculatorTests
     {
         [TestMethod()]
-        public void SumByGroupTest_numberPreGroup_is_3_accessValueFunc_is_Cost_()
+        public void SumByGroupTest_有群未滿_numberPreGroup_is_3_accessValueFunc_is_Cost()
         {
             // arrange
             var calculator = new Calculator();
@@ -31,15 +31,136 @@ namespace ProductTest.Tool
             };
             var numberPreGroup = 3;
             Func<Product, int> accessValueFunc = m => m.Cost;
-            var expected = new int[] { 6, 3, 5 };
+            var expected = new int[] { 6, 15, 24, 21 };
 
             // act
             var result = calculator.SumByGroup<Product>(products, numberPreGroup, accessValueFunc);
-            result = result.ToArray();
+            
+            // assert
+            CollectionAssert.AreEqual(expected, result.ToArray());
+        }
 
-            //
+        [TestMethod()]
+        public void SumByGroupTest_有群未滿_numberPreGroup_is_4_accessValueFunc_is_Revenue()
+        {
+            // arrange
+            var calculator = new Calculator();
+            var products = new List<Product>()
+            {
+                new Product {Id = 1, Cost = 1, Revenue = 11, SellPrice = 21},
+                new Product {Id = 2, Cost = 2, Revenue = 12, SellPrice = 22},
+                new Product {Id = 3, Cost = 3, Revenue = 13, SellPrice = 23},
+                new Product {Id = 4, Cost = 4, Revenue = 14, SellPrice = 24},
+                new Product {Id = 5, Cost = 5, Revenue = 15, SellPrice = 25},
+                new Product {Id = 6, Cost = 6, Revenue = 16, SellPrice = 26},
+                new Product {Id = 7, Cost = 7, Revenue = 17, SellPrice = 27},
+                new Product {Id = 8, Cost = 8, Revenue = 18, SellPrice = 28},
+                new Product {Id = 9, Cost = 9, Revenue = 19, SellPrice = 29},
+                new Product {Id = 10, Cost = 10, Revenue = 20, SellPrice = 30},
+                new Product {Id = 11, Cost = 11, Revenue = 21, SellPrice = 31},
+            };
+            var numberPreGroup = 4;
+            Func<Product, int> accessValueFunc = m => m.Revenue;
+            var expected = new int[] { 50, 66, 60 };
 
-            Assert.Fail();
+            // act
+            var result = calculator.SumByGroup<Product>(products, numberPreGroup, accessValueFunc);
+            var actural = result.ToArray();
+            
+            // assert
+            CollectionAssert.AreEqual(expected, actural);
+        }
+
+        [TestMethod()]
+        public void SumByGroupTest_只有一群_SellPrice_is_SellPrice()
+        {
+            // arrange
+            var calculator = new Calculator();
+            var products = new List<Product>()
+            {
+                new Product {Id = 1, Cost = 1, Revenue = 11, SellPrice = 21},
+                new Product {Id = 2, Cost = 2, Revenue = 12, SellPrice = 22},
+                new Product {Id = 3, Cost = 3, Revenue = 13, SellPrice = 23},
+                new Product {Id = 4, Cost = 4, Revenue = 14, SellPrice = 24},
+                new Product {Id = 5, Cost = 5, Revenue = 15, SellPrice = 25},
+                new Product {Id = 6, Cost = 6, Revenue = 16, SellPrice = 26},
+                new Product {Id = 7, Cost = 7, Revenue = 17, SellPrice = 27},
+                new Product {Id = 8, Cost = 8, Revenue = 18, SellPrice = 28},
+                new Product {Id = 9, Cost = 9, Revenue = 19, SellPrice = 29},
+                new Product {Id = 10, Cost = 10, Revenue = 20, SellPrice = 30},
+                new Product {Id = 11, Cost = 11, Revenue = 21, SellPrice = 31},
+            };
+            var numberPreGroup = 11;
+            Func<Product, int> accessValueFunc = m => m.SellPrice;
+            var expected = new int[] { 286 };
+
+            // act
+            var result = calculator.SumByGroup<Product>(products, numberPreGroup, accessValueFunc);
+            var actural = result.ToArray();
+
+            // assert
+            CollectionAssert.AreEqual(expected, actural);
+        }
+
+        [TestMethod()]
+        public void SumByGroupTest_剛好組成群_SellPrice_is_Cost()
+        {
+            // arrange
+            var calculator = new Calculator();
+            var products = new List<Product>()
+            {
+                new Product {Id = 1, Cost = 1, Revenue = 11, SellPrice = 21},
+                new Product {Id = 2, Cost = 2, Revenue = 12, SellPrice = 22},
+                new Product {Id = 3, Cost = 3, Revenue = 13, SellPrice = 23},
+                new Product {Id = 4, Cost = 4, Revenue = 14, SellPrice = 24},
+                new Product {Id = 5, Cost = 5, Revenue = 15, SellPrice = 25},
+                new Product {Id = 6, Cost = 6, Revenue = 16, SellPrice = 26},
+                new Product {Id = 7, Cost = 7, Revenue = 17, SellPrice = 27},
+                new Product {Id = 8, Cost = 8, Revenue = 18, SellPrice = 28},
+                new Product {Id = 9, Cost = 9, Revenue = 19, SellPrice = 29},
+                new Product {Id = 10, Cost = 10, Revenue = 20, SellPrice = 30},
+            };
+            var numberPreGroup = 5;
+            Func<Product, int> accessValueFunc = m => m.Cost;
+            var expected = new int[] { 15,  40};
+
+            // act
+            var result = calculator.SumByGroup<Product>(products, numberPreGroup, accessValueFunc);
+            var actural = result.ToArray();
+
+            // assert
+            CollectionAssert.AreEqual(expected, actural);
+        }
+
+        [TestMethod()]
+        public void SumByGroupTest_numberPreGroup超過資料量_SellPrice_is_Cost()
+        {
+            // arrange
+            var calculator = new Calculator();
+            var products = new List<Product>()
+            {
+                new Product {Id = 1, Cost = 1, Revenue = 11, SellPrice = 21},
+                new Product {Id = 2, Cost = 2, Revenue = 12, SellPrice = 22},
+                new Product {Id = 3, Cost = 3, Revenue = 13, SellPrice = 23},
+                new Product {Id = 4, Cost = 4, Revenue = 14, SellPrice = 24},
+                new Product {Id = 5, Cost = 5, Revenue = 15, SellPrice = 25},
+                new Product {Id = 6, Cost = 6, Revenue = 16, SellPrice = 26},
+                new Product {Id = 7, Cost = 7, Revenue = 17, SellPrice = 27},
+                new Product {Id = 8, Cost = 8, Revenue = 18, SellPrice = 28},
+                new Product {Id = 9, Cost = 9, Revenue = 19, SellPrice = 29},
+                new Product {Id = 10, Cost = 10, Revenue = 20, SellPrice = 30},
+                new Product {Id = 11, Cost = 11, Revenue = 21, SellPrice = 31},
+            };
+            var numberPreGroup = 16;
+            Func<Product, int> accessValueFunc = m => m.Cost;
+            var expected = new int[] { 66 };
+
+            // act
+            var result = calculator.SumByGroup<Product>(products, numberPreGroup, accessValueFunc);
+            var actural = result.ToArray();
+
+            // assert
+            CollectionAssert.AreEqual(expected, actural);
         }
     }
 }
