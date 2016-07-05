d96492d0b5a7c270cf67e4674a276831516440a8
eugenechen0514 <yuchiehchen@outlook.com>
2016-07-06 01:18:42 +0800

    Add 異常系測試

--- ProductTest/Tool/CalculatorTests.cs
+++ ProductTest/Tool/CalculatorTests.cs
@@ -65,10 +65,10 @@ namespace ProductTest.Tool
 
             // act
             var result = calculator.SumByGroup<Product>(products, numberPreGroup, accessValueFunc);
-            var actural = result.ToArray();
+            var actual = result.ToArray();
             
             // assert
-            CollectionAssert.AreEqual(expected, actural);
+            CollectionAssert.AreEqual(expected, actual);
         }
 
         [TestMethod()]
@@ -96,10 +96,10 @@ namespace ProductTest.Tool
 
             // act
             var result = calculator.SumByGroup<Product>(products, numberPreGroup, accessValueFunc);
-            var actural = result.ToArray();
+            var actual = result.ToArray();
 
             // assert
-            CollectionAssert.AreEqual(expected, actural);
+            CollectionAssert.AreEqual(expected, actual);
         }
 
         [TestMethod()]
@@ -126,10 +126,10 @@ namespace ProductTest.Tool
 
             // act
             var result = calculator.SumByGroup<Product>(products, numberPreGroup, accessValueFunc);
-            var actural = result.ToArray();
+            var actual = result.ToArray();
 
             // assert
-            CollectionAssert.AreEqual(expected, actural);
+            CollectionAssert.AreEqual(expected, actual);
         }
 
         [TestMethod()]
@@ -157,10 +157,69 @@ namespace ProductTest.Tool
 
             // act
             var result = calculator.SumByGroup<Product>(products, numberPreGroup, accessValueFunc);
-            var actural = result.ToArray();
+            var actual = result.ToArray();
 
             // assert
-            CollectionAssert.AreEqual(expected, actural);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SumByGroupTest_輸入參數異常_無元素輸入資料()
+        {
+            // arrange
+            var calculator = new Calculator();
+            var numberPreGroup = 16;
+            Func<Product, int> accessValueFunc = m => m.Cost;
+            
+            // act
+            var actual = calculator.SumByGroup<Product>(new List<Product>(), numberPreGroup, accessValueFunc);
+           
+            // assert
+            Assert.AreEqual(null, actual);
+        }
+
+        [TestMethod()]
+        public void SumByGroupTest_輸入參數異常_空輸入資料()
+        {
+            // arrange
+            var calculator = new Calculator();
+            var numberPreGroup = 16;
+            Func<Product, int> accessValueFunc = m => m.Cost;
+
+            // act
+            var actual = calculator.SumByGroup<Product>(null, numberPreGroup, accessValueFunc);
+
+            // assert
+            Assert.AreEqual(null, actual);
+        }
+
+        [TestMethod()]
+        public void SumByGroupTest_輸入參數異常_非正數numberPreGroup()
+        {
+            // arrange
+            var calculator = new Calculator();
+            var products = new List<Product>()
+            {
+                new Product {Id = 1, Cost = 1, Revenue = 11, SellPrice = 21},
+                new Product {Id = 2, Cost = 2, Revenue = 12, SellPrice = 22},
+                new Product {Id = 3, Cost = 3, Revenue = 13, SellPrice = 23},
+                new Product {Id = 4, Cost = 4, Revenue = 14, SellPrice = 24},
+                new Product {Id = 5, Cost = 5, Revenue = 15, SellPrice = 25},
+                new Product {Id = 6, Cost = 6, Revenue = 16, SellPrice = 26},
+                new Product {Id = 7, Cost = 7, Revenue = 17, SellPrice = 27},
+                new Product {Id = 8, Cost = 8, Revenue = 18, SellPrice = 28},
+                new Product {Id = 9, Cost = 9, Revenue = 19, SellPrice = 29},
+                new Product {Id = 10, Cost = 10, Revenue = 20, SellPrice = 30},
+                new Product {Id = 11, Cost = 11, Revenue = 21, SellPrice = 31},
+            };
+            var numberPreGroup = 0;
+            Func<Product, int> accessValueFunc = m => m.Cost;
+
+            // act
+            var actual = calculator.SumByGroup<Product>(products, numberPreGroup, accessValueFunc);
+
+            // assert
+            Assert.AreEqual(null, actual);
         }
     }
 }
